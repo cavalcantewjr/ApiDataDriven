e8bfaf99cb9c69c69b4ef31538e800bca001c69d
Washington Cavalcante <wfcavalcantejr@outlook.com>
2021-08-08 20:29:02 -0300

    Estrutura base da API

---
+++ Shop.Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Shop.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [MaxLength(60, ErrorMessage = "Este Campo deve conter entre 3 a 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Este Campo deve conter entre 3 a 60 caracteres")]
+        public string Title { get; set; }
+
+    }
+}
---
+++ ShopApi/Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApi.Controllers
+{
+    [Route("categories")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public string Get()
+        {
+            return "GET";
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public string GetById(int id)
+        {
+            return "GET" + id;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public string Post()
+        {
+            return "POST";
+        }
+
+        [HttpPut]
+        [Route("")]
+        public string Put()
+        {
+            return "PUT";
+        }
+
+        [HttpDelete]
+        [Route("")]
+        public string Delete()
+        {
+            return "DELETE";
+        }
+    }
+}
